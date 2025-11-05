620dae770923bd29e2e84db1585d03fad4c0ca1c
Tosin Oguntuyi <tcubed03@gmail.com>
2025-11-05 23:11:16 +0100

    import modules

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
-def main():
-    print("Hello from user-gen-ai-agent!")
+from typing import List
+import json
+import os
+import random
+import string
+from datetime import datetime, timedelta
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage, AIMessage, BaseMessage
+from langchain_core.tools import tool
+from langgraph.prebuilt import create_react_agent
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("OPENAI_API_KEY")
 
 
-if __name__ == "__main__":
-    main()
